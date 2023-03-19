d26bca355e6cd3a5ec2413f3bd4f7e36b1992ea3
Lee Chan <125026241+statschan2@users.noreply.github.com>
2023-03-19 23:35:28 +0900

    Add files via upload

---
+++ Lane_Detection/realsense/detect_distance.py
@@ -0,0 +1,35 @@
+import cv2
+import pyrealsense2
+from realsense_depth import *
+
+point = (400, 300)
+
+def show_distance(event, x, y, args, params):
+    global point
+    point = (x, y)
+
+# Initialize Camera Intel Realsense
+dc = DepthCamera()
+
+# Create mouse event
+cv2.namedWindow("Color frame")
+cv2.setMouseCallback("Color frame", show_distance)
+
+while True:
+    ret, depth_frame, color_frame = dc.get_frame()
+    
+    # flip the image
+    color_frame = cv2.flip(color_frame,1)
+    depth_frame = cv2.flip(depth_frame,1)
+
+    # Show distance for a specific point
+    cv2.circle(color_frame, point, 10, (0, 200, 255), cv2.FILLED)
+    distance = depth_frame[point[1], point[0]]
+
+    cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
+    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
